8912a454d8049446b4aab0c6a08375aeb9ddb4cf
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-12-04 16:15:41 +0530

    task 7

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import ExpenseItems from "./Components/ExpenseItems"
+
+function App() {
+  
+
+  return (
+    <ExpenseItems />
+  )
+}
+
+export default App
---
+++ client/src/Components/ExpenseItems.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ExpenseItems = () => {
+  return (
+    <div>
+        <h1>Expenses</h1>
+        <div>
+            <p>Food : 10rs</p>
+            <p>Petrol : 100rs</p> 
+            <p>Movies : 200rs</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default ExpenseItems
