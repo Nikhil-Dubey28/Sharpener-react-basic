b258f761c07dc282180a6f0f2040f6b32cc2319d
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-12-15 00:12:00 +0530

    advance react task 1

--- client/src/Components/Expenses/ExpenseItems.jsx
+++ client/src/Components/Expenses/ExpenseItems.jsx
@@ -6,12 +6,16 @@ import Card from '../UI/Card'
 
 const ExpenseItems = ({ expense }) => {
 
+  const handleClick = () => {
+console.log('clicked')
+  }
+
   return (
     <Card className='expense-item'>
       {/* <h1>Expenses</h1> */}
       <ExpenseDate date={expense.date} />
       <ExpenseDetails title={expense.title} amount={expense.amount} />
-
+      <button onClick={() => handleClick()}></button>
 
 
     </Card>
