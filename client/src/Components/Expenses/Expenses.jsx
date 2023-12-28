9a33f0ec272ab764bc6cdedf392361123609f45c
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-12-28 16:27:50 +0530

    task 11 -filter

--- client/src/Components/Expenses/Expenses.jsx
+++ client/src/Components/Expenses/Expenses.jsx
@@ -12,6 +12,12 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -19,8 +25,17 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {/* {props.items.map((expense) => (
           <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))} */}
+
+{filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
